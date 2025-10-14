bfd1c9232ebb330c1495927b1023174ecba03366
avel0041 <vel-ar@mail.ru>
2025-10-14 21:08:29 +0300

    11

--- EGE tasks/24/24_9.py
+++ EGE tasks/24/24_9.py
@@ -1,2 +1,37 @@
-# В текстовом файле k7-m22.txt находится цепочка из прописных (заглавных) символов латинского алфавита A, B, C, D, E, F. Найдите количество подцепочек из трех символов, в которых символы идут в обратном алфавитном порядке и индекс первой буквы последней найденной подцепочки (первый символ исходной цепочки имеет индекс 0).
-#Например, у цепочки FDCBA таких подцепочек три: FDC, DCB и CBA, а индекс первой буквы последней найденной подцепочки (CBA) два и, следовательно, ответ: 3 2.
+c=open('24-181.txt').readline()
+m=0
+for i in range(len(c)):
+    for r in range(i+m, len(c)):
+        f=c[i:r+1]
+        if (f.count('A')+f.count('E')+f.count('I')+f.count('O')+f.count('U')+f.count('Y'))>7 or f.count('.')>=1:
+            break
+        else:
+            m=max(m, len(f))
+print(m)
+
+
+f=open('24-181.txt').readline()
+f = f.replace('A', 'Y')
+f = f.replace('E', 'Y')
+f = f.replace('I', 'Y')
+f = f.replace('O', 'Y')
+f = f.replace('U', 'Y')
+m=0
+c1 = 0
+c2 = 0
+l=0
+
+for i in range(len(f)):
+    if f[i]=='Y':
+        c1+=1
+    if f[i]=='.':
+        c2+=1
+    while c1>7 or c2>0:
+        if f[l]=='Y':
+            c1-=1
+        if f[l]=='.':
+            c2-=1
+        l+=1
+    m = max(i-l+1, m)
+    
+print(m)        
